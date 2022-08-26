563f94434b50b14a70594684058e6267dd4d0349
siniy <nanimonodemonai@gmail.com>
2022-08-26 11:49:41 +0900

    カテゴリーの削除機能を実装

--- lib/queryCategory.php
+++ lib/queryCategory.php
@@ -31,6 +31,17 @@ class QueryCategory extends connect
         $stmt->execute();
     }
 
+    public function delete()
+    {
+        $id = $this->category->getId();
+        $stmt = $this->dbh->prepare("UPDATE articles SET category_id = NULL WHERE category_id=:id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $stmt = $this->dbh->prepare("DELETE FROM categories WHERE id=:id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
     public function find($id)
     {
         $stmt = $this->dbh->prepare("SELECT * FROM categories WHERE id=:id");
@@ -71,6 +82,13 @@ class Category
         $queryCategory = new QueryCategory();
         $queryCategory->setCategory($this);
         $queryCategory->save();
+    }
+
+    public function delete()
+    {
+        $queryCategory = new QueryCategory();
+        $queryCategory->setCategory($this);
+        $queryCategory->delete();
     }
 
     public function getId()
