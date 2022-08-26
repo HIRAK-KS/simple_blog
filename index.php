b24dac84530a5dd2e9cfbef9a64a07f83fb09ec5
siniy <nanimonodemonai@gmail.com>
2022-08-26 12:10:44 +0900

    カテゴリー別アーカイブ機能を実装

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 include 'lib/connect.php';
 include 'lib/queryArticle.php';
 include 'lib/article.php';
+include 'lib/queryCategory.php';
+
+// ===== ↓Queryクラスのインスタンス生成、メニュー準備を追加 ここから↓ =====
+$queryArticle = new QueryArticle();
+$queryCategory = new QueryCategory();
+
+// メニューの準備
+$monthly = $queryArticle->getMonthlyArchiveMenu();
+$category = $queryCategory->getCategoryMenu();
+// ===== ↑Queryクラスのインスタンス生成、メニュー準備を追加 ここまで↑ =====
 
 $limit = 5;
 $page = 1;
 $month = null;
 $title = "";
+$category_id = null;
 
 // ページ数の決定
 if (!empty($_GET['page']) && intval($_GET['page']) > 0) {
@@ -19,9 +30,18 @@ if (!empty($_GET['month'])) {
   $title = $month . 'の投稿一覧';
 }
 
-$queryArticle = new QueryArticle();
-$pager = $queryArticle->getPager($page, $limit, $month);
-$monthly = $queryArticle->getMonthlyArchiveMenu();
+// カテゴリー別
+if (isset($_GET['category'])) {
+  if (isset($category[$_GET['category']])) {
+    $title = 'カテゴリー：' . $category[$_GET['category']]['name'];
+    $category_id = intval($_GET['category']);
+  } else {
+    $title = 'カテゴリーなし';
+    $category_id = 0;
+  }
+}
+
+$pager = $queryArticle->getPager($page, $limit, $month, $category_id);
 ?>
 
 <!doctype html>
@@ -96,7 +116,7 @@ $monthly = $queryArticle->getMonthlyArchiveMenu();
           <nav aria-label="Page navigation example">
             <ul class="pagination">
               <?php for ($i = 1; $i <= ceil($pager['total'] / $limit); $i++) : ?>
-                <li class="page-item"><a class="page-link" href="index.php?page=<?php echo $i ?><?php echo $month ? '&month=' . $month : '' ?>"><?php echo $i ?></a></li>
+                <li class="page-item"><a class="page-link" href="index.php?page=<?php echo $i ?><?php echo $month ? '&month=' . $month : '' ?><?php echo !is_null($category_id) ? '&category=' . $category_id : '' ?>"><?php echo $i ?></a></li>
               <?php endfor ?>
             </ul>
           </nav>
@@ -114,6 +134,15 @@ $monthly = $queryArticle->getMonthlyArchiveMenu();
           <ol class="list-unstyled mb-0">
             <?php foreach ($monthly as $m) : ?>
               <li><a href="index.php?month=<?php echo $m['month'] ?>"><?php echo $m['month'] ?> (<?php echo $m['count'] ?>)</a></li>
+            <?php endforeach ?>
+          </ol>
+        </div>
+
+        <div class="p-4">
+          <h4>カテゴリ別アーカイブ</h4>
+          <ol class="list-unstyled mb-0">
+            <?php foreach ($category as $c) : ?>
+              <li><a href="index.php?category=<?php echo $c['id'] ? $c['id'] : 0 ?>"><?php echo $c['name'] ? $c['name'] : 'カテゴリーなし' ?>(<?php echo $c['count'] ?>)</a></li>
             <?php endforeach ?>
           </ol>
         </div>
